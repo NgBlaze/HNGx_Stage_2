0ed862355e74fe938ba0ad497544478e1f237fe2
Precious <okolaaprecious@gmail.com>
2023-09-13 11:55:09 +0100

    Updated code base

--- restapi_project/person/views.py
+++ restapi_project/person/views.py
@@ -1,4 +1,3 @@
-# myapi/views.py
 from rest_framework import generics
 from .models import Person
 from .serializers import PersonSerializer
@@ -8,6 +7,9 @@ class PersonListCreateView(generics.ListCreateAPIView):
     serializer_class = PersonSerializer
 
     def get_queryset(self):
+        name = self.request.query_params.get('name')
+        if name:
+            return Person.objects.filter(name__iexact=name)
         return Person.objects.none()
 
     def perform_create(self, serializer):
